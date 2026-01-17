703829d7416ec9aa2203fd253d536dcf25d2f510
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 01:32:57 +0300

    Update thumbnail URL in menu message

--- commands/main/menu.js
+++ commands/main/menu.js
@@ -25,7 +25,7 @@ module.exports = {
 
     await sock.sendMessage(from, { 
       text: menu,
-      contextInfo: { externalAdReply: { title: "STANYTZ BOT ACTIVE", body: "WRONG TURN 6", mediaType: 1, thumbnailUrl: "https://files.catbox.moe/0v7rju.jpg" }}
+      contextInfo: { externalAdReply: { title: "STANYTZ BOT ACTIVE", body: "WRONG TURN 6", mediaType: 1, thumbnailUrl: "https://chatgpt.com/s/m_696c0db153ac8191886a3b6a250c00d6" }}
     });
   }
 };
