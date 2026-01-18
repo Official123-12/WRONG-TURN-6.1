ad393c42e8a35dfbff9ada5e2d969019fdc62989
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 23:29:43 +0300

    Update ytmp4.js

--- commands/media/ytmp4.js
+++ commands/media/ytmp4.js
@@ -2,10 +2,17 @@ const axios = require('axios');
 module.exports = {
     name: 'ytmp4',
     async execute(m, sock, commands, args) {
-        if (!args[0]) return m.reply("Provide YouTube Link.");
+        const from = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(from, { text: "Provide a YouTube link." }, { quoted: m });
         try {
             const res = await axios.get(`https://api.dhammasepun.me/api/ytmp4?url=${args[0]}`);
-            await sock.sendMessage(m.key.remoteJid, { video: { url: res.data.result.download_url }, caption: "WRONG TURN 6 | STANYTZ" }, { quoted: m });
-        } catch (e) { m.reply("Download failed."); }
+            const videoUrl = res.data.result.download_url;
+            await sock.sendMessage(from, { 
+                video: { url: videoUrl }, 
+                caption: `WRONG TURN 6 ✔️\nDeveloper: STANYTZ` 
+            }, { quoted: m });
+        } catch (e) {
+            await sock.sendMessage(from, { text: "Failed to download video." }, { quoted: m });
+        }
     }
 };
