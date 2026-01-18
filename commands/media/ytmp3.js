71588ea82d9940bb591a41242d56ce7dd57c62f6
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 22:54:07 +0300

    Update ytmp3.js

--- commands/media/ytmp3.js
+++ commands/media/ytmp3.js
@@ -2,10 +2,13 @@ const axios = require('axios');
 module.exports = {
     name: 'ytmp3',
     async execute(m, sock, commands, args) {
-        if (!args[0]) return m.reply("Provide YouTube Link.");
+        const jid = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(jid, { text: "Provide YouTube Link." }, { quoted: m });
         try {
             const res = await axios.get(`https://api.dhammasepun.me/api/ytmp3?url=${args[0]}`);
-            await sock.sendMessage(m.key.remoteJid, { audio: { url: res.data.result.download_url }, mimetype: 'audio/mp4' }, { quoted: m });
-        } catch (e) { m.reply("Download failed."); }
+            await sock.sendMessage(jid, { audio: { url: res.data.result.download_url }, mimetype: 'audio/mp4' }, { quoted: m });
+        } catch (e) { 
+            await sock.sendMessage(jid, { text: "Failed to process audio." }, { quoted: m });
+        }
     }
 };
