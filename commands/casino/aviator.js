94b948fc4a7a4841c997937e9c2979725c2003e5
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 22:53:21 +0300

    Update aviator.js

--- commands/casino/aviator.js
+++ commands/casino/aviator.js
@@ -1,7 +1,9 @@
 module.exports = {
     name: 'aviator',
-    async execute(m) {
+    async execute(m, sock) {
+        const jid = m.key.remoteJid;
         const mult = (Math.random() * 5 + 1).toFixed(2);
-        m.reply(`🚀 *AVIATOR PREDICTOR*\n\n┃ 📊 *Next Fly:* ${mult}x\n┃ 🛡️ *Accuracy:* 88%\n┗━━━━━━━━━━━━┛\n*DEV: STANYTZ*`);
+        const res = `🚀 *AVIATOR PREDICTOR*\n\n┃ 📊 *Next Fly:* ${mult}x\n┃ 🛡️ *Accuracy:* 88%\n┗━━━━━━━━━━━━┛\n*WRONG TURN 6 | STANYTZ INDUSTRIES* ✔️`;
+        await sock.sendMessage(jid, { text: res }, { quoted: m });
     }
 };
