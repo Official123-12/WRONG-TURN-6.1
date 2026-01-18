ff617e24dfd5726aafbbe450bab5c0ffe088d27d
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 22:52:30 +0300

    Update mines.js

--- commands/casino/mines.js
+++ commands/casino/mines.js
@@ -1,13 +1,14 @@
 module.exports = {
     name: 'mines',
-    async execute(m) {
+    async execute(m, sock) {
+        const jid = m.key.remoteJid;
         const icons = ["💣", "💎", "💎", "💎", "💎", "💣", "💎", "💎", "💎"];
         const grid = icons.sort(() => Math.random() - 0.5);
-        let res = `🎰 *WT6 MINES*\n\n` +
+        let res = `🎰 *WT6 MINES GAME*\n\n` +
                   `| ${grid[0]} | ${grid[1]} | ${grid[2]} |\n` +
                   `| ${grid[3]} | ${grid[4]} | ${grid[5]} |\n` +
                   `| ${grid[6]} | ${grid[7]} | ${grid[8]} |\n\n` +
-                  `*CAPTION BY STANYTZ*`;
-        m.reply(res);
+                  `*WRONG TURN 6 | STANYTZ INDUSTRIES* ✔️`;
+        await sock.sendMessage(jid, { text: res }, { quoted: m });
     }
 };
