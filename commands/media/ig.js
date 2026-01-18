24d1a07abdad513f5a3535a7c9aa3bc2d53c8cbd
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 23:33:40 +0300

    Update ig.js

--- commands/media/ig.js
+++ commands/media/ig.js
@@ -2,10 +2,17 @@ const axios = require('axios');
 module.exports = {
     name: 'ig',
     async execute(m, sock, commands, args) {
-        if (!args[0]) return m.reply("Paste Instagram Link.");
+        const from = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(from, { text: "Provide an Instagram link." }, { quoted: m });
         try {
             const res = await axios.get(`https://api.dhammasepun.me/api/igdl?url=${args[0]}`);
-            await sock.sendMessage(m.key.remoteJid, { video: { url: res.data.result[0].url }, caption: "STANYTZ INSTA DL" }, { quoted: m });
-        } catch (e) { m.reply("Content might be private."); }
+            const mediaUrl = res.data.result[0].url;
+            await sock.sendMessage(from, { 
+                video: { url: mediaUrl }, 
+                caption: `WRONG TURN 6 ✔️\nDeveloper: STANYTZ` 
+            }, { quoted: m });
+        } catch (e) {
+            await sock.sendMessage(from, { text: "Error. Content might be private or invalid." }, { quoted: m });
+        }
     }
 };
