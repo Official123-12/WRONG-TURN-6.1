c96ad5a74e351d9898bdf026ec96577e872e4ec6
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 23:29:01 +0300

    Update fb.js

--- commands/media/fb.js
+++ commands/media/fb.js
@@ -2,10 +2,17 @@ const axios = require('axios');
 module.exports = {
     name: 'fb',
     async execute(m, sock, commands, args) {
-        if (!args[0]) return m.reply("Paste Facebook Link.");
+        const from = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(from, { text: "Provide a Facebook link." }, { quoted: m });
         try {
             const res = await axios.get(`https://api.dhammasepun.me/api/fbdl?url=${args[0]}`);
-            await sock.sendMessage(m.key.remoteJid, { video: { url: res.data.result.hd }, caption: "STANYTZ FB DL" }, { quoted: m });
-        } catch (e) { m.reply("Error fetching video."); }
+            const videoUrl = res.data.result.hd || res.data.result.sd;
+            await sock.sendMessage(from, { 
+                video: { url: videoUrl }, 
+                caption: `WRONG TURN 6 ✔️\nDeveloper: STANYTZ` 
+            }, { quoted: m });
+        } catch (e) {
+            await sock.sendMessage(from, { text: "Error fetching Facebook video." }, { quoted: m });
+        }
     }
 };
