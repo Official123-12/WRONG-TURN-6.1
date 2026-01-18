3f6888f34157cae0810616afade921396fe03c94
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 23:30:25 +0300

    Update ytmp3.js

--- commands/media/ytmp3.js
+++ commands/media/ytmp3.js
@@ -2,13 +2,18 @@ const axios = require('axios');
 module.exports = {
     name: 'ytmp3',
     async execute(m, sock, commands, args) {
-        const jid = m.key.remoteJid;
-        if (!args[0]) return sock.sendMessage(jid, { text: "Provide YouTube Link." }, { quoted: m });
+        const from = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(from, { text: "Provide a YouTube link." }, { quoted: m });
         try {
             const res = await axios.get(`https://api.dhammasepun.me/api/ytmp3?url=${args[0]}`);
-            await sock.sendMessage(jid, { audio: { url: res.data.result.download_url }, mimetype: 'audio/mp4' }, { quoted: m });
-        } catch (e) { 
-            await sock.sendMessage(jid, { text: "Failed to process audio." }, { quoted: m });
+            const audioUrl = res.data.result.download_url;
+            await sock.sendMessage(from, { 
+                audio: { url: audioUrl }, 
+                mimetype: 'audio/mp4',
+                caption: `WRONG TURN 6 ✔️\nDeveloper: STANYTZ`
+            }, { quoted: m });
+        } catch (e) {
+            await sock.sendMessage(from, { text: "Failed to convert audio." }, { quoted: m });
         }
     }
 };
