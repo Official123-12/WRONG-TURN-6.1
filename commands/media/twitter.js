438ce06ace1f1fcf22e963adfbd1176da9fc83e3
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 23:42:59 +0300

    Update twitter.js

--- commands/media/twitter.js
+++ commands/media/twitter.js
@@ -1,1 +1,18 @@
-
+const axios = require('axios');
+module.exports = {
+    name: 'twitter',
+    async execute(m, sock, commands, args) {
+        const from = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(from, { text: "Provide a Twitter link." }, { quoted: m });
+        try {
+            const res = await axios.get(`https://api.dhammasepun.me/api/twitter?url=${args[0]}`);
+            const videoUrl = res.data.result.SD || res.data.result.HD;
+            await sock.sendMessage(from, { 
+                video: { url: videoUrl }, 
+                caption: `WRONG TURN 6 ✔️\nDeveloper: STANYTZ` 
+            }, { quoted: m });
+        } catch (e) {
+            await sock.sendMessage(from, { text: "Failed to download Twitter video." }, { quoted: m });
+        }
+    }
+};
