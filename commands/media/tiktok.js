e8a022691ce760283420d1ab6408e5c3996a3317
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 22:54:57 +0300

    Update tiktok.js

--- commands/media/tiktok.js
+++ commands/media/tiktok.js
@@ -2,11 +2,14 @@ const axios = require('axios');
 module.exports = {
     name: 'tiktok',
     async execute(m, sock, commands, args) {
-        if (!args[0]) return m.reply("Paste TikTok Link.");
+        const jid = m.key.remoteJid;
+        if (!args[0]) return sock.sendMessage(jid, { text: "Provide TikTok URL." }, { quoted: m });
         try {
             const res = await axios.get(`https://api.tiklydown.eu.org/api/download?url=${args[0]}`);
-            const vid = res.data.video.noWatermark;
-            await sock.sendMessage(m.key.remoteJid, { video: { url: vid }, caption: "WRONG TURN 6 ✔️\n*DEV: STANYTZ*" }, { quoted: m });
-        } catch (e) { m.reply("Invalid Link."); }
+            const videoUrl = res.data.video.noWatermark;
+            await sock.sendMessage(jid, { video: { url: videoUrl }, caption: "*WRONG TURN 6 | STANYTZ INDUSTRIES* ✔️" }, { quoted: m });
+        } catch (e) { 
+            await sock.sendMessage(jid, { text: "Download failed." }, { quoted: m });
+        }
     }
 };
