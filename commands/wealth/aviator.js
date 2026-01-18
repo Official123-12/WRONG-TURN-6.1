5fce5392a47e0f6941490f42fff1aed94d540434
Loving Programming <officialstanlee143@gmail.com>
2026-01-18 16:51:59 +0300

    Update aviator.js

--- commands/wealth/aviator.js
+++ commands/wealth/aviator.js
@@ -1,7 +1,1 @@
-module.exports = {
-    name: "aviator",
-    async execute(sock, msg, args) {
-        const crash = (Math.random() * 3 + 1.2).toFixed(2);
-        await sock.sendMessage(msg.key.remoteJid, { text: `✈️ *AVIATOR AI PREDICTION:*\n\nNext crash: *${crash}x*\n\n_Powered by WRONG TURN 6_` });
-    }
-};
+
