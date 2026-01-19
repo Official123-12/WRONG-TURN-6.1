dcab4df1bc393993a165ffc9d3ff90cc6e2f0a9a
Loving Programming <officialstanlee143@gmail.com>
2026-01-19 19:17:09 +0300

    Update stats.js

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -2,15 +2,24 @@ const os = require('os');
 
 module.exports = {
     name: 'stats',
-    async execute(m, sock) {
+    async execute(m, sock, commands, args, db, forwardedContext) {
         const from = m.key.remoteJid;
         
-        const totalMem = (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2);
-        const freeMem = (os.freemem() / (1024 * 1024 * 1024)).toFixed(2);
-        const cpuModel = os.cpus()[0].model;
-        
-        const msg = `WRONG TURN 6 ✔️\nDeveloper: STANYTZ\n\nPlatform: ${os.platform()}\nRAM: ${totalMem}GB\nFree RAM: ${freeMem}GB\nCPU: ${cpuModel}`;
-        
-        await sock.sendMessage(from, { text: msg }, { quoted: m });
+        const ramTotal = (os.totalmem() / 1024 / 1024 / 1024).toFixed(2);
+        const ramFree = (os.freemem() / 1024 / 1024 / 1024).toFixed(2);
+        const platform = os.platform().toUpperCase();
+        const cpu = os.cpus()[0].model.split(' ')[0];
+
+        let res = `╭─── • 🥀 • ───╮\n`;
+        res += `   ꜱʏꜱᴛᴇᴍ  ꜱᴛᴀᴛꜱ   \n`;
+        res += `╰─── • 🥀 • ───╯\n\n`;
+        msg += `⚘  *ᴘʟᴀᴛꜰᴏʀᴍ* : ${platform}\n`;
+        msg += `⚘  *ᴄᴘᴜ* : ${cpu}\n`;
+        msg += `⚘  *ʀᴀᴍ* : ${ramFree}ɢʙ / ${ramTotal}ɢʙ\n`;
+        msg += `⚘  *ᴄᴍᴅꜱ* : ${commands.length}\n`;
+        msg += `⚘  *ʟɪʙ* : ᴀɴɢᴜʟᴀʀꜱᴏᴄᴋᴇᴛꜱ\n\n`;
+        msg += `_ᴅᴇᴠᴇʟᴏᴘᴇʀ: ꜱᴛᴀɴʏᴛᴢ_`;
+
+        await sock.sendMessage(from, { text: msg, contextInfo: forwardedContext }, { quoted: m });
     }
 };
