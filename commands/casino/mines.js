77deb89445890411ebcd2850e5cd201fba1d64dd
Loving Programming <officialstanlee143@gmail.com>
2026-01-19 11:37:07 +0300

    Update mines.js

--- commands/casino/mines.js
+++ commands/casino/mines.js
@@ -1,14 +1,20 @@
 module.exports = {
     name: 'mines',
-    async execute(m, sock) {
-        const jid = m.key.remoteJid;
+    async execute(m, sock, commands, args, db, forwardedContext) {
         const icons = ["💣", "💎", "💎", "💎", "💎", "💣", "💎", "💎", "💎"];
         const grid = icons.sort(() => Math.random() - 0.5);
-        let res = `🎰 *WT6 MINES GAME*\n\n` +
-                  `| ${grid[0]} | ${grid[1]} | ${grid[2]} |\n` +
-                  `| ${grid[3]} | ${grid[4]} | ${grid[5]} |\n` +
-                  `| ${grid[6]} | ${grid[7]} | ${grid[8]} |\n\n` +
-                  `*WRONG TURN 6 | STANYTZ INDUSTRIES* ✔️`;
-        await sock.sendMessage(jid, { text: res }, { quoted: m });
+
+        let body = `┏━━━━━━━━━━━━━━━━━━━━━━━━┓\n`;
+        body += `┃  🎰 𝗪𝗧𝟲 𝗠𝗜𝗡𝗘𝗦 𝗚𝗔𝗠𝗘   ┃\n`;
+        body += `┣━━━━━━━━━━━━━━━━━━━━━━━━┫\n`;
+        body += `┃    | ${grid[0]} | ${grid[1]} | ${grid[2]} |\n`;
+        body += `┃    | ${grid[3]} | ${grid[4]} | ${grid[5]} |\n`;
+        body += `┃    | ${grid[6]} | ${grid[7]} | ${grid[8]} |\n`;
+        body += `┣━━━━━━━━━━━━━━━━━━━━━━━━┫\n`;
+        body += `┃ 🥀 𝗗𝗲𝘃: 𝗦𝗧𝗔𝗡𝗬𝗧𝗭\n`;
+        body += `┃ 🥀 𝗟𝘂𝗰𝗸: ${Math.floor(Math.random() * 100)}%\n`;
+        body += `┗━━━━━━━━━━━━━━━━━━━━━━━━┛`;
+
+        await sock.sendMessage(m.key.remoteJid, { text: body, contextInfo: forwardedContext }, { quoted: m });
     }
 };
