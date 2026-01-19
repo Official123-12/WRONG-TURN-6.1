46811e8efe76df8b3222ed85de778173c6c5e92d
Loving Programming <officialstanlee143@gmail.com>
2026-01-19 14:24:12 +0300

    Update video.js

--- commands/media/video.js
+++ commands/media/video.js
@@ -1,1 +1,16 @@
-
+const axios = require('axios');
+module.exports = {
+    name: 'video',
+    async execute(m, sock, commands, args, db, forwardedContext) {
+        const query = args.join(" ");
+        if (!query) return m.reply("ᴡʜᴀᴛ ᴠɪᴅᴇᴏ ᴅᴏ ʏᴏᴜ ɴᴇᴇᴅ?");
+        try {
+            const search = await axios.get(`https://api.popcat.xyz/ytsearch?q=${encodeURIComponent(query)}`);
+            const vid = search.data[0];
+            const dl = await axios.get(`https://api.dhammasepun.me/api/ytmp4?url=${vid.url}`);
+            
+            let cap = `╭─── • 🥀 • ───╮\n  ᴠ ɪ ᴅ ᴇ ᴏ  ᴅ ʟ  \n╰─── • 🥀 • ───╯\n\n│ ◦ ᴛɪᴛʟᴇ: ${vid.title}\n│ ◦ ᴅᴇᴠ: ꜱᴛᴀɴʏᴛᴢ\n└──────────────`;
+            await sock.sendMessage(m.key.remoteJid, { video: { url: dl.data.result.download_url }, caption: cap, contextInfo: forwardedContext });
+        } catch (e) { m.reply("ᴠɪᴅᴇᴏ ɴᴏᴛ ꜰᴏᴜɴᴅ."); }
+    }
+};
