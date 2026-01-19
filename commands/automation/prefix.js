217bbaa468389bc381e44080375e6684b75c1ef7
Loving Programming <officialstanlee143@gmail.com>
2026-01-19 18:55:26 +0300

    Update prefix.js

--- commands/automation/prefix.js
+++ commands/automation/prefix.js
@@ -4,18 +4,16 @@ module.exports = {
     name: 'prefix',
     async execute(m, sock, commands, args, db, forwardedContext) {
         const ownerId = sock.user.id.split(':')[0];
-        if (!m.key.fromMe && !m.sender.startsWith(ownerId)) return;
+        if (!m.sender.startsWith(ownerId)) return;
 
         const newPrefix = args[0];
-        if (!newPrefix || newPrefix.length > 1) return m.reply("Provide a single character prefix.");
+        if (!newPrefix || newPrefix.length > 1) return m.reply("ᴘʀᴏᴠɪᴅᴇ ᴀ ꜱɪɴɢʟᴇ ᴄʜᴀʀᴀᴄᴛᴇʀ (ᴇ.ɢ. !)");
 
         await setDoc(doc(db, "SETTINGS", "GLOBAL"), { prefix: newPrefix }, { merge: true });
 
-        let res = `╭─── • 🥀 • ───╮\n`;
-        res += `  ᴘ ʀ ᴇ ꜰ ɪ x  ꜱ ᴇ ᴛ  \n`;
-        res += `╰─── • 🥀 • ───╯\n\n`;
-        res += `🥀  ɴᴇᴡ ᴘʀᴇꜰɪx: [ ${newPrefix} ]\n\n`;
-        res += `_ᴅᴇᴠᴇʟᴏᴘᴇʀ: ꜱᴛᴀɴʏᴛᴢ_`;
+        let res = `╭─── • 🥀 • ───╮\n  ᴘʀᴇꜰɪx ᴜᴘᴅᴀᴛᴇ  \n╰─── • 🥀 • ───╯\n\n`;
+        res += `🥀 *ɴᴇᴡ ᴘʀᴇꜰɪx* : [ ${newPrefix} ]\n\n`;
+        res += `_ʙᴏᴛ ᴡɪʟʟ ɴᴏᴡ ʀᴇꜱᴘᴏɴᴅ ᴛᴏ ${newPrefix}_`;
 
         await sock.sendMessage(m.key.remoteJid, { text: res, contextInfo: forwardedContext });
     }
