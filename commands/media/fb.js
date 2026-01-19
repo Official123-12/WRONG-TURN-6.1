a26ad7d9e5957f87f99f0463dce9b7ab5bae8025
Loving Programming <officialstanlee143@gmail.com>
2026-01-19 14:26:51 +0300

    Update fb.js

--- commands/media/fb.js
+++ commands/media/fb.js
@@ -1,18 +1,13 @@
 const axios = require('axios');
 module.exports = {
     name: 'fb',
-    async execute(m, sock, commands, args) {
-        const from = m.key.remoteJid;
-        if (!args[0]) return sock.sendMessage(from, { text: "Provide a Facebook link." }, { quoted: m });
+    async execute(m, sock, commands, args, db, forwardedContext) {
+        if (!args[0]) return m.reply("ᴘʀᴏᴠɪᴅᴇ ꜰᴀᴄᴇʙᴏᴏᴋ ʟɪɴᴋ.");
         try {
             const res = await axios.get(`https://api.dhammasepun.me/api/fbdl?url=${args[0]}`);
-            const videoUrl = res.data.result.hd || res.data.result.sd;
-            await sock.sendMessage(from, { 
-                video: { url: videoUrl }, 
-                caption: `WRONG TURN 6 ✔️\nDeveloper: STANYTZ` 
-            }, { quoted: m });
-        } catch (e) {
-            await sock.sendMessage(from, { text: "Error fetching Facebook video." }, { quoted: m });
-        }
+            const vid = res.data.result.hd || res.data.result.sd;
+            let cap = `╭─── • 🥀 • ───╮\n  ꜰ ʙ  ᴅ ᴏ ᴡ ɴ ʟ ᴏ ᴀ ᴅ  \n╰─── • 🥀 • ───╯\n\n_ᴅᴇᴠ: ꜱᴛᴀɴʏᴛᴢ_`;
+            await sock.sendMessage(m.key.remoteJid, { video: { url: vid }, caption: cap, contextInfo: forwardedContext });
+        } catch (e) { m.reply("ꜰᴀɪʟᴇᴅ ᴛᴏ ꜰᴇᴛᴄʜ ᴠɪᴅᴇᴏ."); }
     }
 };
